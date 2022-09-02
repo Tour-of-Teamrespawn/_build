654017f5775ce16cb9c52436c1a6d97e4e5ee110
Andy455 <andy.dlp93@gmail.com>
2022-09-02 19:14:16 +0100

    Force TLS1.2 initially

--- build.ps1
+++ build.ps1
@@ -40,6 +40,7 @@ param (
 if ($PSCmdlet.ShouldProcess('This script', 'Update script with latest version from GitHub')) {
 
     Write-Host "Getting and comparing GitHub script with this script..."
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $NewScriptContents = (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Tour-of-Teamrespawn/_build/main/build.ps1' -ErrorAction 'Stop').Content
 
     $CurrentScriptContents = Get-Content -Path $MyInvocation.MyCommand.Path -Raw
